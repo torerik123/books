481f7af0cfe01a9b1e73dc17f5d147b6dfbe6b8a
torerik123 <tor_erik_grimen@hotmail.com>
2020-09-28 15:51:24 +0200

    created tables for database

---
+++ import.py
@@ -0,0 +1,19 @@
+import csv
+import os
+
+from models import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    f = open("books.csv")
+    reader = csv.reader(f)
+    for isbn, title, author, year in reader:
+        
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
