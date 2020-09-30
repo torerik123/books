f40b6a3cf045d4e54dc9209a0e20c25da47c6407
torerik123 <tor_erik_grimen@hotmail.com>
2020-09-30 17:16:02 +0200

    imported to DB

--- import.py
+++ import.py
@@ -1,18 +1,29 @@
 import csv
 import os
 
+from flask import Flask, render_template, request
 from models import *
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 def main():
     f = open("books.csv")
     reader = csv.reader(f)
+    
+    #book = Book(isbn=2222, title="mybook", author="Hasnsss", year="1992")    
+    #db.session.add(book)
+    #db.session.commit()
+
     for isbn, title, author, year in reader:
-        
+        if isbn or title or author or year is None:
+            continue
+        book = Book(isbn=isbn, title=title, author=author, year=year)
+        db.session.add(book)
+        #print(f"ISBN: {isbn}")
+    db.session.commit()       
 
 if __name__ == "__main__":
     with app.app_context():
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class Book(db.Model):
     __tablename__ = "books"
     id = db.Column(db.Integer, primary_key=True)
-    isbn = db.Column(db.Integer, nullable=False)
+    isbn = db.Column(db.String, nullable=False)
     title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer, nullable=False)
