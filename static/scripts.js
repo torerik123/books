f7ed1e92bf043815989cedffeeedeebf9782644b
torerik123 <tor_erik_grimen@hotmail.com>
2020-10-11 09:24:49 +0200

    added loop through isbns for slider imgs

--- static/scripts.js
+++ static/scripts.js
@@ -1,19 +1,27 @@
-// Only load on index page
+// TODO: Get top 10 NY times bestsellers
+const isbns = [9780244951344, 9789291209910, 9788293516163, 9781560657699 , 9780198527565, 9780521421881 ,9780520221581, 9780719057298 , 9780307485779, 9788256019878];
 
-// Get JSON response from Google Books API
-fetch('https://www.googleapis.com/books/v1/volumes/RJxWIQOvoZUC')
+let img_nr = 0;
+
+// For every element in isbns list
+isbns.forEach(isbn => {
+  
+  // Get JSON response from Google Books API
+  fetch('https://www.googleapis.com/books/v1/volumes?q=isbn=' + isbn)
   .then(response => response.json())
   .then(data => {
     
-    // Imagelink from volumeinfo
-    const image = data['volumeInfo']['imageLinks']['thumbnail'];
+    // Get thumbnail image
+    const image = data['items'][0]['volumeInfo']['imageLinks']['thumbnail'];
 
-    
-    const carousel = document.querySelectorAll("#slider");
+    // Get ID of slider element
+    const carousel = document.querySelector("#slider" + img_nr);
 
-    carousel.forEach(element => {
-      element.src = image;  
+    // Set source to image link
+    carousel.src = image;  
+    img_nr++;
     });
-    
   });
+
+
 
